df01ce3eea31a0b3505fe1fecc123dd721d95659
JeferssonTrejos <jeffersonbladimirlovo@gmail.com>
2024-06-03 15:37:56 -0600

    fix

--- index.js
+++ index.js
@@ -3,12 +3,11 @@ const path = require('path')
 const app = express();
 const PORT = 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.get('/ping', (req, res) => {
     res.json({ ping: 'pong' })
 })
-
 
 app.listen(PORT, () => {
     console.log(`Corriendo en http://localhost:${PORT}`);
