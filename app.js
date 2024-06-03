66d3a08001666d35153f855f2d95fbc663f207cf
JeferssonTrejos <jeffersonbladimirlovo@gmail.com>
2024-06-03 15:17:24 -0600

    add project

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use('/',express.static('public'))
+
+app.get('/ping', (req, res) => {
+    res.json({ ping: 'pong' })
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Corriendo en http://localhost:${PORT}`);
+})
+
+
